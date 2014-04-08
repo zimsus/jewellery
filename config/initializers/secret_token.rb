64a537def7dcb4b5a9d56501c97a5a9c7cabe5f4
Stan Zima <zimsus@gmail.com>
2014-04-08 02:23:16 +0100

    Added Welcome Constructor

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jewellery::Application.config.secret_key_base = '35cdeeb5306dde178718f6b290705a90b8358ea4e152b82ec3fec626b5b89b82f2b8f02e2b6df0fd731ca055c63b3e2d26938cd92ba27f9faa46ef00a054bd63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jewellery::Application.config.session_store :cookie_store, key: '_jewellery_session'
